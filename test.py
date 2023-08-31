8c42538752cd3cc1ff9c252e65a7c6d28253cd88
OceanPC\ocean <ocean.yeung@deeptranlate.hk>
2023-08-31 18:35:38 +0900

    output the zh and en link

---
+++ test.py
@@ -0,0 +1,13 @@
+from sentence_transformers import util
+import requests
+
+
+def getEmbedding():
+    sentences = {'sentences': ['Anti-Financial Investment Fraud Zone',
+                            '防制金融投資詐騙專區']}
+    embeddings = requests.post('http://192.168.4.101:8000/embedding', json=sentences).json()
+
+    cosine_scores = util.cos_sim(embeddings['embeddings'][0], embeddings['embeddings'][1])
+    return "{:.4f}".format(cosine_scores[0][0])
+
+print(getEmbedding())
--- webDataCawler/spiders/webRealtekSemiconductor.py
+++ webDataCawler/spiders/webRealtekSemiconductor.py
@@ -1,10 +1,37 @@
 import scrapy
 
-
 class WebrealteksemiconductorSpider(scrapy.Spider):
     name = "webRealtekSemiconductor"
-    allowed_domains = ["www.realtek.com"]
-    start_urls = ["https://www.realtek.com/en/"]
+    allowed_domains = ["www.tpex.org.tw"]
+    start_urls = ["https://www.tpex.org.tw/web/index.php?l=zh-tw"]
 
     def parse(self, response):
-        pass
+        links = response.xpath('//a/@href').getall()
+
+        # get links
+        newLinks = []
+        for link in links:
+            if '?l=en-us' in link or '?l=ja-jp' in link:
+                continue
+            if link[0] == '/':
+                newLinks.append(f'https://{self.allowed_domains[0]}{link}')
+
+        for index, link in enumerate(newLinks):
+            if index > 10:
+                break
+            yield scrapy.Request(link, callback=self.parsePage)
+
+    def parsePage(self, response):
+        zhLink = response.url
+        enLink = zhLink.replace('?l=zh-tw', '?l=en-us')
+
+        
+
+        return {
+            'zh': zhLink,
+            'en': enLink
+        }
+
+
+
+
